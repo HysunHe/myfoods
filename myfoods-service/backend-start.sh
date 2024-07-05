de002b27e166e79cdbd4bab6d543f2a382ed5e1c
Hysun He <hysunhe@foxmail.com>
2024-07-05 16:03:09 +0800

    Update backend-start.sh

--- myfoods-service/backend-start.sh
+++ myfoods-service/backend-start.sh
@@ -4,7 +4,7 @@ docker run -d \
     --restart=always \
     --name=gateway \
     -p 8080:8080 \
-    -e 'DB_CONN_STR=jdbc:oracle:thin:@//132.145.xxx.xxx:1521/BotDevDB_iad1hb.sub01110857210.devopsvcn.oraclevcn.com' \
-    -e 'DB_USER=c##botadmin' \
-    -e 'DB_PASS=BotWelcome123$$' \
+    # -e 'DB_CONN_STR=jdbc:oracle:thin:@//132.145.xxx.xxx:1521/BotDevDB_iad1hb.sub01110857210.devopsvcn.oraclevcn.com' \
+    # -e 'DB_USER=c##botadmin' \
+    # -e 'DB_PASS=BotWelcome123$$' \
     hysunhe/botgateway:latest
